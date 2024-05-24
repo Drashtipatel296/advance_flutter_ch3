e42b05afc100d0c463988685460c27c4aecaaaa5
Admin <drashtipatel7080@gmail.com>
2024-05-24 13:43:12 +0530

    24-02-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:advance_flutter_ch3/screens/Chrome_flutter_inappwebview%20Package/provider/indicator_provider.dart';
+import 'package:advance_flutter_ch3/screens/Chrome_flutter_inappwebview%20Package/screens/home_screen.dart';
 import 'package:advance_flutter_ch3/screens/connectivity_plus_package/connectivity_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => IndicatorProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstScreen(),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/screens/Chrome_flutter_inappwebview Package/screens/components/bottomnavigarion_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../home_screen.dart';
+
+Row bottomNavigationPage() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      IconButton(onPressed: () {
+        inAppWebViewController.goBack();
+      }, icon: const Icon(Icons.arrow_back_ios)),
+      IconButton(onPressed: () {
+        inAppWebViewController.goForward();
+      }, icon: const Icon(Icons.arrow_forward_ios)),
+      IconButton(onPressed: () {
+        inAppWebViewController.reload();
+      }, icon: const Icon(Icons.refresh)),
+      IconButton(onPressed: () {
+
+      }, icon: const Icon(Icons.add_box_outlined)),
+      IconButton(onPressed: () {
+
+      }, icon: const Icon(Icons.home)),
+    ],
+  );
+}
